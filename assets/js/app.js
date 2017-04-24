e92a20abcbcb20251aeffdab4e2d6d7d9bc3c8e9
rccacho <rccacho@gmail.com>
2017-04-24 13:27:38 -0700

    adjusted icon links and contact area

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,2 @@
 // sanity check
 console.log("app.js linked.");
-
-var work = [
-	{
-		"link": "http://teacher-archives.herokuapp.com/";
-	},
-	{
-		"link": "http://doge-meetings.herokuapp.com/";
-	},
-	{
-		"link": "http://vagabond-irk.herokuapp.com/";
-	},
-	{
-		"link": "https://desolate-wave-59412.herokuapp.com/";
-	}];
-
-
-// goal: loop over the array above to change the work links on the DOM
-
-for (var i = 0; i < work.length; i++) {
-	$("img").attr("src", work.link[i]);
-}
